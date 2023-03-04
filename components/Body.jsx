6f96eb6b3d45c05dae4bbcb03aed90e3bd9a901a
AnmaritoS <95002766+AnmaritoS@users.noreply.github.com>
2023-03-04 16:29:53 -0500

    search bar done, added divider between buttons and filter and added react hook search setSearch for searchbar

--- components/Body.jsx
+++ components/Body.jsx
@@ -1,9 +1,11 @@
+import { useState } from "react";
 import Search from "./Search.jsx";
 
 function Body() {
+  const [search, setSearch] = useState("");
   return (
     <div className="bg-black ml-24 py-4 space-y-8 md:max-w-6xl flex-grow md:mr-5">
-      <Search />
+      <Search search={search} setSearch={setSearch} />
     </div>
   );
 }
